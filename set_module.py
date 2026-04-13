e7acc2ab5dc944eb1f99c0744d4bcdf25919cd13
Lakshana-2025 <ravichandran04061980_bcs29@mepcoeng.ac.in>
2026-04-13 17:30:19 +0530

    Create set_module.py

---
+++ set_module.py
@@ -0,0 +1,11 @@
+def sunion(s1, s2):
+    return s1.union(s2)
+
+def sintersection(s1, s2):
+    return s1.intersection(s2)
+
+def sdifference(s1, s2):
+    return s1.difference(s2)
+
+def ssymmetricdiff(s1, s2):
+    return s1.symmetric_difference(s2)
